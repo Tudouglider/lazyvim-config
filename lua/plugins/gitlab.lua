7ebf7135b9640a4c9352ed3cce5e261ea45efe2f
Guanzhong.li <ligz@clounix.com>
2026-03-14 21:56:42 +0800

    correct gitlab key map

--- lua/plugins/gitlab.lua
+++ lua/plugins/gitlab.lua
@@ -27,31 +27,23 @@ return {
       })
     end,
     keys = {
-      -- 主入口
-      { "<leader>G", "<cmd>GitLab<cr>", desc = "GitLab" },
-      
-      -- MR 操作
-      { "<leader>Gl", "<cmd>GitLab mr list<cr>", desc = "List MRs" },
-      { "<leader>Gr", "<cmd>GitLab mr review<cr>", desc = "Review MR" },
-      { "<leader>Gc", "<cmd>GitLab mr create<cr>", desc = "Create MR" },
-      { "<leader>Gv", "<cmd>GitLab mr view<cr>", desc = "View MR" },
-      { "<leader>Ga", "<cmd>GitLab mr approve<cr>", desc = "Approve MR" },
-      { "<leader>Gm", "<cmd>GitLab mr merge<cr>", desc = "Merge MR" },
-      
-      -- Issue
-      { "<leader>Gi", "<cmd>GitLab issue list<cr>", desc = "List Issues" },
-      { "<leader>Gt", "<cmd>GitLab issue create<cr>", desc = "New Issue" },
-      
-      -- Pipeline
-      { "<leader>Gp", "<cmd>GitLab pipeline<cr>", desc = "Pipelines" },
-      
-      -- 其他
-      { "<leader>Go", "<cmd>GitLab open<cr>", desc = "Open in Browser" },
-      { "<leader>Gs", "<cmd>GitLab snippet<cr>", desc = "Snippets" },
-      
-      -- 独立 diffview
-      { "<leader>Gd", "<cmd>DiffviewOpen<cr>", desc = "Diffview" },
-      { "<leader>Gf", "<cmd>DiffviewFileHistory<cr>", desc = "File History" },
-    },
+       -- 主入口：选择 MR
+       { "<leader>G", function() require("gitlab").choose_merge_request() end, desc = "GitLab: Choose MR" },
+
+       -- MR 操作
+	  { "<leader>Gl", function() require("gitlab").choose_merge_request() end, desc = "List MRs" },  -- 复用选择器
+	  { "<leader>Gr", function() require("gitlab").review() end, desc = "Review MR" },
+	  { "<leader>Gc", function() require("gitlab").create_mr() end, desc = "Create MR" },
+	  { "<leader>Ga", function() require("gitlab").approve() end, desc = "Approve MR" },
+	  { "<leader>Gm", function() require("gitlab").merge() end, desc = "Merge MR" },
+
+	  -- 其他功能
+	  { "<leader>Go", function() require("gitlab").open_in_browser() end, desc = "Open in Browser" },
+	  { "<leader>Gp", function() require("gitlab").pipeline() end, desc = "Pipelines" },
+
+	  -- Diffview 相关（独立插件，保持原命令）
+	  { "<leader>Gd", "<cmd>DiffviewOpen<cr>", desc = "Diffview" },
+	  { "<leader>Gf", "<cmd>DiffviewFileHistory<cr>", desc = "File History" },
+    }, 
   },
 }
