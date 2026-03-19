d035196de4184e472610c39ef006a6bfd508ad74
li.guanzhong <ligz@clounix.com>
2026-03-19 22:52:45 +0800

    fix molten

--- lua/plugins/molten.lua
+++ lua/plugins/molten.lua
@@ -7,9 +7,11 @@ return {
     { "<leader>me", ":MoltenEvaluateOperator<CR>", desc = "Evaluate Operator (Run)" },
     { "<leader>mv", ":<C-u>MoltenEvaluateVisual<CR>gv", mode = "v", desc = "Evaluate Visual (Run Selection)" },
     { "<leader>mr", ":MoltenReevaluateCell<CR>", desc = "Re-evaluate Cell" },
+    { "<leader>mo", ":noautocmd MoltenEnterOutput<CR>", desc = "Enter Output Window" },
     { "<leader>md", ":MoltenDelete<CR>", desc = "Delete Output" },
     { "<leader>mh", ":MoltenHideOutput<CR>", desc = "Hide/Show Output" },
-    { "<leader>mo", ":noautocmd MoltenEnterOutput<CR>", desc = "Enter Output Window" },
+    { "<leader>mc", ":MoltenHideOutput<CR>", desc = "Clear/Hide All Output" },
+    { "<leader>mX", ":MoltenHideOutput<CR>:noautocmd MoltenDeinit<CR>:MoltenInit<CR>", desc = "Hard Reset Molten UI" },
   },
   init = function()
     -- 注册 Which-key 菜单组名称
@@ -21,9 +23,9 @@ return {
     end)
 
     -- 针对 M4 Pro 的基础性能配置
-    vim.g.molten_auto_open_output = false     -- 运行后不自动跳到输出窗口，保持焦点在代码
+    vim.g.molten_auto_open_output = true     -- 运行后不自动跳到输出窗口，保持焦点在代码
     vim.g.molten_output_win_max_height = 16   -- 调高输出窗口高度，方便看量化指标
-    vim.g.molten_wrap_output = true           -- 自动换行
-    vim.g.molten_virt_text_output = true      -- 在行尾显示虚拟文本输出
+    vim.g.molten_wrap_output = true          -- 自动换行
+    vim.g.molten_virt_text_output = false      -- 在行尾显示虚拟文本输出
   end,
 }
