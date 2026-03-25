3eb181f4e7a1c07fc5d9e3ba9fe3b12756f9f448
Hei.Tudou <tudouglider@gmail.com>
2026-03-26 07:10:45 +0800

    show hidden files in snack

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,7 +6,21 @@ return {
     image = { enabled = true },
     -- 如果你还需要其他模块，也可以在这里开启
     -- bigfile = { enabled = true },
-    -- dashboard = { enabled = true },
+    dashboard = { enabled = true },
+    picker = {
+      enabled = true,
+      sources = {
+        explorer = {
+          hidden = true,
+          ignored = true,
+          layout = { preset = "sidebar", position = "left" }, -- 侧边栏布局
+        },
+      },
+      -- 让文件选择器也显示隐藏文件
+      files = { hidden = true },
+      -- grep 时也包含隐藏文件
+      grep = { hidden = true },
+    },
     -- 等等
   },
 }
